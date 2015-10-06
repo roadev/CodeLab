c30abcfbfa8db5fa62288d5d69a19ba0cb6b323e
Juan Roa <jdroa92@gmail.com>
2015-10-06 11:11:33 -0500

    finishing MisCentavos

--- MisCentavos.java
+++ MisCentavos.java
@@ -1,7 +1,28 @@
 import javax.swing.JOptionPane;
 
 public class MisCentavos {
-  public static void main(String[] args){
 
+  // public void MisCentavos(){
+  //
+  // }
+
+  public double calc(double vp){
+    double vf = vp * ((1 + 0.098)^24);
+    return vf;
+  }
+
+  public void get(double vp){
+    vp = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el valor de la consignación: "));
+  }
+
+  public void show(){
+    JOptionPane.showMessageDialog(null, "**Banco Mis Centavos** \n Valor consignado      Valor futuro en 2 años \n " + vp + "      " + vf);
+  }
+
+  public static void main(String[] args){
+    MisCentavos app = new MisCentavos();
+    app.get();
+    app.calc();
+    app.show();
   }
 }
