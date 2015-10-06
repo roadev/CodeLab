1f869efc1a029ff8660cbc21158fafd504c39621
Juan Roa <jdroa92@gmail.com>
2015-10-06 17:47:58 -0500

    Finished operations

--- Operations.java
+++ Operations.java
@@ -1,24 +1,29 @@
 import javax.swing.JOptionPane;
 public class Operations{
 
-  double y, z, w;
+  double a, y, z, w = 0;
   int x = 0;
-  String valY3 = " ";
+  String valY3 = "";
+  String valY5 = "";
     public void calc(){
       while(x <= 10){
         y = (Math.pow(x,2) - (2*x));
         z = z + y;
 
-        if (z % 3 == 0){
-          valY3 += z + " , ";
-          w = z + w;
+        if (y % 3 == 0){
+          valY3 += y + " \n ";
+          w = y + w;
+        }
+        if (y % 10 == 5){
+          valY5 += y + " \n ";
+          a = y + a;
         }
         x++;
 
       }
-
       JOptionPane.showMessageDialog(null, "Suma de todos los Y: " + z + "\n" + "Suma de los valores de Y múltiplos de 3: " + w +
-      "\n "+"Valores de Y múltiplos de 3 " + valY3);
+      "\n "+"Valores de Y múltiplos de 3 " +"\n"+ valY3 + "\n" + "Valores de Y cuyo último dígito sea 5: " +"\n"+ valY5 + "\n" +
+      "Suma de los valores de Y cuyo último dígito sea 5: " + a);
   }
 
   public static void main(String[] args) {
