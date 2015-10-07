d164d86f2bd6234269999c72cd0d67b608f7ce68
Juan Roa <jdroa92@gmail.com>
2015-10-06 23:06:13 -0500

    Students fixed

--- Students.java
+++ Students.java
@@ -1,12 +1,10 @@
 import javax.swing.JOptionPane;
 public class Students{
-  int a, b, c, d;
+  int a=0, b=0, c=0, d=0;
   double calc=0;
   public void calc(){
 
     for(int j = 1; j<=10; j++){
-
-
       calc = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la calificación del estudiante "+(j)));
       if((calc >= 1)||(calc <= 100)){
 
@@ -26,9 +24,8 @@ public class Students{
     }
     JOptionPane.showMessageDialog(null, "La cantidad de estudiantes que obtuvieron calificación < 50: "+ a + "\n" +
     "La cantidad de estudiantes que obtuvieron calificación < 50: " + b + "\n" + "La cantidad de estudiantes que obtuvieron calificación >= 50 y < 80: " + b +
-    "La cantidad de estudiantes que obtuvieron calificación >= 70 y < 80: " + c + "\n" + "La cantidad de estudiantes que obtuvieron calificación >= 80: " + d +
-    "\n" + "");
-
+    "\n"+"La cantidad de estudiantes que obtuvieron calificación >= 70 y < 80: " + c + "\n" + "La cantidad de estudiantes que obtuvieron calificación >= 80: " + d +
+    "\n");
 
   }
   public static void main(String[] args){
