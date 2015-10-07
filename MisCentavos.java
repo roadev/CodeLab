e51e58132819f8e1f7242e207fbbfdc468af6308
Juan Roa <jdroa92@gmail.com>
2015-10-06 23:44:08 -0500

    Doc added to MisCentavos

--- MisCentavos.java
+++ MisCentavos.java
@@ -1,24 +1,36 @@
+/*
+* Este programa recibe un valor real de una consignación, calcula un interés + capital
+* e imprime el resultado final debido.
+*/
 import javax.swing.JOptionPane;
-
+/**
+*
+* @authors Juan Roa, Fabian Santacruz, Victor Uzurriaga
+* @version 0.1
+* @since 2015-10-01
+*/
 public class MisCentavos {
 
-  // public void MisCentavos(){
-  //
-  // }
-
+  /**
+  * Método get donde se recibe el valor presente y se ingresa al método calc()
+  */
   public void get(){
     double vp = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el valor de la consignación: "));
     this.calc(vp);
   }
-
+  /**
+  * Método que recibe el valor de vp ingresado en la función anterior y calcula el valor futuro, además, lo imprime en un JOptionPane
+  */
   public void calc(double vp){
     double vf = vp * (Math.pow((1 + 0.098),24));
     JOptionPane.showMessageDialog(null, "**Banco Mis Centavos** \n Valor consignado      Valor futuro en 2 años \n " + vp + "      " + vf);
   }
 
   public static void main(String[] args){
+    /**
+    * Se crea un objeto llamado app y se llama el método get()
+    */
     MisCentavos app = new MisCentavos();
     app.get();
-    // app.calc();
   }
 }
