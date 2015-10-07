1fc01dc20201d43222d0ad5c9fe291a0cdf47702
Juan Roa <jdroa92@gmail.com>
2015-10-06 23:23:01 -0500

    Doc finished

--- Students.java
+++ Students.java
@@ -29,8 +29,13 @@ public class Students{
     */
     for(int j = 1; j<=10; j++){
       calc = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la calificación del estudiante "+(j)));
+      /**
+      * El primer condicinal general proporciona realizar el proceso si cumple la condición dada
+      */
       if((calc >= 1)&&(calc <= 100)){
-
+        /**
+        * Condicionales if para realizar el proceso solicitado
+        */
         if(calc < 50){
           a++;
         }else if(calc >= 50 && calc < 80){
@@ -40,11 +45,17 @@ public class Students{
         }else if(calc >= 80){
           d++;
         }
+        /**
+        * Éste else permite mostrar un mensaje de error al no cumplise el if general
+        */
         }else{
           JOptionPane.showMessageDialog(null, "¡Debe ingresar valores entre 1 y 100!");
           break;
       }
     }
+    /**
+    * Aquí se imprimen los datos del proceso
+    */
     JOptionPane.showMessageDialog(null, "La cantidad de estudiantes que obtuvieron calificación < 50: "+ a + "\n" +
     "La cantidad de estudiantes que obtuvieron calificación < 50: " + b + "\n" + "La cantidad de estudiantes que obtuvieron calificación >= 50 y < 80: " + b +
     "\n"+"La cantidad de estudiantes que obtuvieron calificación >= 70 y < 80: " + c + "\n" + "La cantidad de estudiantes que obtuvieron calificación >= 80: " + d +
@@ -52,6 +63,9 @@ public class Students{
 
   }
   public static void main(String[] args){
+    /**
+    * Se crea un objeto llamado app y se llama el método calc()
+    */
     Students app = new Students();
     app.calc();
   }
