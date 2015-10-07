bddbd1ebbec5ff9c65ec86fe6b0a345fe3fd77a6
Juan Roa <jdroa92@gmail.com>
2015-10-06 23:19:11 -0500

    Fixing students and adding documentation

--- Students.java
+++ Students.java
@@ -1,12 +1,35 @@
+/*
+* Este programa muestra cantidades totales dependiendo de condiciones
+* dadas para las calificaciones de los estudiantes
+*/
+
 import javax.swing.JOptionPane;
+/**
+*
+* @authors Juan Roa, Fabian Santacruz, Victor Uzurriaga
+* @version 0.1
+* @since 2015-10-01
+*/
 public class Students{
+  /**
+  * @a, b, c, d -> son los acumumladores de cantidades
+  * @calc es la variable que recibe los 10 valores
+  */
+
   int a=0, b=0, c=0, d=0;
   double calc=0;
+
+  /**
+  * Éste método hace las operaciones y recibe los datos de la variable calc
+  */
   public void calc(){
 
+    /**
+    * Éste ciclo repite desde 1 hasta 10 el proceso de pedir la variable calc
+    */
     for(int j = 1; j<=10; j++){
       calc = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la calificación del estudiante "+(j)));
-      if((calc >= 1)||(calc <= 100)){
+      if((calc >= 1)&&(calc <= 100)){
 
         if(calc < 50){
           a++;
@@ -16,10 +39,10 @@ public class Students{
           c++	;
         }else if(calc >= 80){
           d++;
+        }
         }else{
           JOptionPane.showMessageDialog(null, "¡Debe ingresar valores entre 1 y 100!");
           break;
-        }
       }
     }
     JOptionPane.showMessageDialog(null, "La cantidad de estudiantes que obtuvieron calificación < 50: "+ a + "\n" +
