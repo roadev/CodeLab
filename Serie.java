58101bc68c6081733ddd447b4820df86d1407355
Juan Roa <jdroa92@gmail.com>
2015-10-06 23:27:35 -0500

    Doc finished

--- Serie.java
+++ Serie.java
@@ -1,11 +1,29 @@
+/*
+* Éste programa muestra una serie sencilla
+*/
+
 import javax.swing.JOptionPane;
-
+/**
+*
+* @authors Juan Roa, Fabian Santacruz, Victor Uzurriaga
+* @version 0.1
+* @since 2015-10-01
+*/
 public class Serie{
-
+  /**
+  * @a -> Cadena vacía, acumulará los datos a mostrar
+  * @y -> acumulador de la serie
+  */
   int y = 0;
   String a = "";
 
+  /**
+  * Método que calcula e imprime la serie
+  */
   public void calc(){
+    /**
+    * Ciclo desde 1 hasta 20 que acumulará la variable y
+    */
     for (int x = 1; x <= 20; x++){
       a += "\n" +y;
       y = x + y;
@@ -14,7 +32,9 @@ public class Serie{
   }
 
   public static void main(String[] args) {
-
+    /**
+    * Se crea un objeto llamado app y se llama el método calc()
+    */
     Serie app = new Serie();
     app.calc();
 
